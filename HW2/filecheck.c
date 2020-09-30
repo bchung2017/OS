533fed6b38045e1f74a57fc48ced2c268f1cb8fe
Brian <bchung2017@gmail.com>
2020-09-30 17:45:28 -0400

    hw2 final edits

--- HW2/filecheck.c
+++ HW2/filecheck.c
@@ -29,18 +29,20 @@ int main(int argc, char * argv[]) {
 	strcpy(startDirectoryName, argv[1]);
 	readDir(startDirectoryName);
 
-	printf("inode types: ");	
-	for(int i = 0; i < 15; i++)
-	{
-		printf("%d ", inodeTypes[i]);
-	}
-	printf("\n");
+	printf("inode types:\n");
+	printf("S_IFREG: %d\n", inodeTypes[0]);
+	printf("S_IFDIR: %d\n", inodeTypes[1]);
+	printf("S_IFLNK: %d\n", inodeTypes[2]);
+	printf("S_IFCHR: %d\n", inodeTypes[3]);
+	printf("S_IFBLK: %d\n", inodeTypes[4]);	
+	printf("S_IFIFO: %d\n", inodeTypes[5]);	
+	printf("S_IFSOCK: %d\n", inodeTypes[6]);	
 
-	printf("numOfFileBytes: %lld\n", numOfFileBytes);
-	printf("numOfFileBlocks: %lld\n", numOfFileBlocks);
-	printf("numOfMultiLinkInodes: %ld\n", numOfMultiLinkInodes);
-	printf("numOfInvalidSymLinks: %d\n", numOfInvalidSymLinks);
-	printf("numOfProblematicDirNames: %d\n", numOfProblematicDirNames);
+	printf("Total number of file bytes: %lld\n", numOfFileBytes);
+	printf("Total number of file blocks: %lld\n", numOfFileBlocks);
+	printf("Total number of inodes with multiple links: %ld\n", numOfMultiLinkInodes);
+	printf("Total number of invalid sym links: %d\n", numOfInvalidSymLinks);
+	printf("Total number of problematic directory names: %d\n", numOfProblematicDirNames);
 
 }
 
@@ -50,14 +52,21 @@ void readDir(char *dn) {
 	struct stat st;
 
 
-	readStat(dn);
+	switch(readStat(dn)) {
+		case -1:
+			fprintf(stderr, "Error reading file: %s\n", dn);
+			return;
+		case 1:
+			break;
+		case 2:
+			return;
+		}
 	errno = 0;
 
 
 	if (!(dirp=opendir(dn)))
 	{
-		fprintf(stderr, "Not a directory: %s\n", dn);
-		//printf("strerror: %s\n\n", strerror(errno));
+		fprintf(stderr, "Error opening file: %s\n", dn);
 		return;
 	}
 	errno = 0;
@@ -65,24 +74,15 @@ void readDir(char *dn) {
 	
 	while (de=readdir(dirp))
 	{
-		printf("inode name: %s\n", de->d_name);
 		if (strcmp(de->d_name, ".") != 0 && strcmp(de->d_name, "..") != 0) {
 			char nextDir [BUFSIZE]= "";
-			printf("nextDir address: %p\n", &nextDir);
 			strcat(strcat(strcat(nextDir, dn), "/"), de->d_name);
-
-			printf("de->d_name: %s\n", de->d_name);
-			printf("next directory being examined: %s\n\n", nextDir);
 			readDir(nextDir);
-			//printf("name: %s\n", de->d_name);
-		}
-		else {
-			printf("\n");
 		}
 	}
 	if(errno)
 	{
-		fprintf(stderr, "Error reading file\n");
+		fprintf(stderr, "Error reading directory: %s\n", dn);
 	}
 	closedir(dirp);
 	return;
@@ -90,8 +90,10 @@ void readDir(char *dn) {
 
 int readStat(char *dn) {
 	struct stat st;
-	if(stat(dn, &st) != 0)
-		return -1;
+	char buffer[BUFSIZE] = "";
+	if(stat(dn, &st) != 0) {
+		fprintf(stderr, "Cannot stat file %s\n", dn);	
+	}
 	for(int i = 0; i < (sizeof(dn)/sizeof(dn[0])); i++) {
 		if(!(isprint(dn[i])||isspace(dn[i]))) {
 			numOfProblematicDirNames++;
@@ -99,61 +101,45 @@ int readStat(char *dn) {
 		}	
 	}
 	
-	printf("inode: %ld\n", (long) st.st_ino);
-	printf("link count: %ld\n", (long) st.st_nlink);
 	if(st.st_nlink > 2) {
-		printf("multilinkinode: %s\n", dn);
 		numOfMultiLinkInodes++;
 	}
-	printf("mode: %lo (octal)\n", (unsigned long) st.st_mode);
 
 
-	printf("inode type: ");
 	switch(st.st_mode & S_IFMT) {
 		case S_IFREG:  
-    		printf("regular file\n");    
     		readFileInfo(st);     
     		inodeTypes[0]++;
-    		break;
-    	case S_IFDIR:  
-    		printf("directory\n");   
+    		return 2;
+    	case S_IFDIR:   
     		inodeTypes[1]++;
-    		break;
-    	case S_IFLNK:  
-    		printf("symlink\n");  
-    		char buffer[BUFSIZE] = "";
-    		if(readlink(dn, buffer, BUFSIZE) == -1)
-    			numOfInvalidSymLinks++;    
+    		return 1;
+    	case S_IFLNK:   	
+    		if(readlink(dn, buffer, BUFSIZE) == -1) {
+    			numOfInvalidSymLinks++;
+    			memset(buffer, 0, sizeof(buffer));    
+    		}
 			inodeTypes[2]++;           
-    		break;
+    		return 1;
     	case S_IFCHR:  
-    		printf("character device\n");
     		inodeTypes[3]++;          
-			break;
+			return 1;
 		case S_IFBLK:  
-			printf("block device\n");
 			inodeTypes[4]++;              
-			break;
+			return 1;
     	case S_IFIFO:  
-    		printf("FIFO/pipe\n");
     		inodeTypes[5]++;                 
-    		break;  	
+    		return 1;  	
     	case S_IFSOCK: 
-    		printf("socket\n");
     		inodeTypes[6]++;                    
-    		break;
-    	default:       
-    		printf("unknown?\n");                
-    		break;
+    		return 1;
+    	default:                  
+    		return -1;
 	}
-	// fd=open("output1.txt", O_RDONLY);
-	// fstat(fd, &st);	
+	
 }
 
 void readFileInfo(struct stat st) {
-	printf("bytes allocated: %lld\n", (long long) st.st_size);
-	printf("blocks allocated: %lld\n", (long long) st.st_blocks);
-
 	numOfFileBytes += (long long) st.st_size;
 	numOfFileBlocks += (long long) st.st_blocks;
 }
